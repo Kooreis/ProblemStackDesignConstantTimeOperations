211529bc39a320bdb966bd5118c0af747ff7d3c7
Kooreis <kooreis.official@gmail.com>
2024-08-08 23:24:02 -0500

    Implementation of the 'push' method. It adds an element to the stack and updates the min_stack and max_stack if necessary.

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
-        self.min_stack = []
-        self.max_stack = []
+def push(self, x):
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+        if not self.max_stack or x >= self.max_stack[-1]:
+            self.max_stack.append(x)
